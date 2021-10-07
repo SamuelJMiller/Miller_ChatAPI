40a925225a9da4a489089aee71b862d4d203124d
Sam Miller <sam@itsecureadmin.com>
2021-10-07 11:21:44 -0700

    setup empty routes

--- routes/chat.js
+++ routes/chat.js
@@ -4,7 +4,28 @@ let router = express.Router();
 let fs = require('fs');
 
 router.get('/', (req, res) => {
-    res.send('Success!');
+    let rawdata = fs.readFileSync('data.json');
+    let messages = JSON.parse(rawdata);
+
+    console.log(messages);
+
+    res.status(200).json(messages);
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.patch('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
 });
 
 module.exports = router;
